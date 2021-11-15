765f2f1be45254c71fff603e457ab31562bd4708
Marija H <93047044+mh1251@users.noreply.github.com>
2021-11-15 20:53:15 +0100

    Add files via upload

---
+++ routeCalculator.js
@@ -0,0 +1,62 @@
+function calculate(route) {
+    let operations = ['$', '*', '+', '-'];
+    let splitedRoute = []
+    let number = '';
+  
+    if(route.length)
+    for (let i = 0; i < route.length; i++) {
+        let currentElement = route[i]
+        if (operations.indexOf(currentElement) != -1) {
+            splitedRoute.push(+number)
+            splitedRoute.push(currentElement)
+            number = ''
+        } else {
+            number += currentElement
+        }
+    }
+    splitedRoute.push(number)
+  
+
+    for (let i = 0; i < splitedRoute.length; i++) {
+        let current = splitedRoute[i]
+        let result;
+
+        if (current === '$') {
+            result = +splitedRoute[i - 1] / +splitedRoute[i + 1];
+            splitedRoute.splice(i - 1, 3, result);
+            i = 0
+        }
+
+        if (splitedRoute[i] === '*') {
+            result = +splitedRoute[i - 1] * +splitedRoute[i + 1];
+            splitedRoute.splice(i - 1, 3, result)
+            i = 0
+        }
+
+        if (splitedRoute.indexOf('$','*')===-1) {
+            if (splitedRoute[i] === '+') {
+                result = +splitedRoute[i - 1] + +splitedRoute[i + 1];
+                splitedRoute.splice(i - 1, 3, result)
+                i = 0
+            }
+            if (splitedRoute[i] === '-') {
+                result = +splitedRoute[i - 1] - +splitedRoute[i + 1];
+                splitedRoute.splice(i - 1, 3, result)
+                i = 0
+            }
+        }
+        else {
+            continue
+        }
+    }
+
+
+    if (isNaN(+splitedRoute)) {
+        return '400: Bad request'
+    } else {
+        return +splitedRoute
+    }
+
+}
+console.log(calculate(''))
+
