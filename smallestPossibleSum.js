9dd1f7861cda54c0ed921f8d262b3516899b378b
Marija H <93047044+mh1251@users.noreply.github.com>
2021-11-28 19:17:55 +0100

    Add files via upload

---
+++ smallestPossibleSum.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+    while (true) {
+        numbers.sort((a, b) => a - b);
+        console.log(numbers)
+        if (numbers[0] === numbers[numbers.length - 1]) {
+           break;
+        }
+
+        for (let i = 1; i <= numbers.length - 1; i++) {
+            if (numbers[i] > numbers[0]) {
+                numbers[i] = numbers[i] - numbers[0];
+            }
+        }
+
+    }
+    return numbers.reduce((acc, n) => acc + n);
+}
+console.log(solution([6, 2, 3]))
