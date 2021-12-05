eb7aa1b3a8496fdaea7d802073666f951ea16159
Marija H <93047044+mh1251@users.noreply.github.com>
2021-12-05 21:03:26 +0100

    Update countConnectedComponents.js

--- countConnectedComponents.js
+++ countConnectedComponents.js
@@ -1,3 +1,19 @@
+/*You are managing an amazing city and indeed its a maze. This city has the particularity to be divided into districts which are unreachable from the other districts.
+More precisely, a district consists of one or more places connected with roads.
+If there is two places p1 and p2 and there is a path (not necessarily direct) between p1 and p2 then p1 and p2 are in the same district.
+
+As a manager, your objective is to count the number of district.
+
+To perform this task you have for each place in the city the list of the other places that are directly reachable through a street. For example if you are given the city
+
+var city = { 
+  'p0': ['p1', 'p2'],
+  'p1': ['p0'],
+  'p2': ['p0'],
+  'p3': []
+}*/
+
+
 function countDistricts(city){
     var nbDistricts = 0;
     
