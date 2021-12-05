7e91ef3a9ca93d44d0d25b5f0e4ee94f18ed9544
Marija H <93047044+mh1251@users.noreply.github.com>
2021-12-05 22:12:01 +0100

    Update hammingNumbers.js

--- hammingNumbers.js
+++ hammingNumbers.js
@@ -1,3 +1,16 @@
+/*A Hamming number is a positive integer of the form 2i3j5k, for some non-negative integers i, j, and k.
+
+Write a function that computes the nth smallest Hamming number.
+
+Specifically:
+
+The first smallest Hamming number is 1 = 203050
+The second smallest Hamming number is 2 = 213050
+The third smallest Hamming number is 3 = 203150
+The fourth smallest Hamming number is 4 = 223050
+The fifth smallest Hamming number is 5 = 203051*/
+
+
 //helper function that evaluates if a number is a hamming number (example 10/2 -> 5/5 -> 1 vrakja true)
 function isHammingNumber(num) {
     while (num % 5 === 0) num /= 5;
