033f8ee18472d9b1be263e0a3d4baad454b1fa52
Marija H <93047044+mh1251@users.noreply.github.com>
2021-12-06 21:17:20 +0100

    Update magnetParticlesInBox.js

--- magnetParticlesInBox.js
+++ magnetParticlesInBox.js
@@ -9,4 +9,6 @@ function doubles(maxk, maxn) {
   doubles(1, 10);
 
 // https://www.codewars.com/kata/56c04261c3fcf33f2d000534
+// v(k,n) = 1/k(n+1)^2k 
+// k - rows, n-columns, v-force by particle
 
