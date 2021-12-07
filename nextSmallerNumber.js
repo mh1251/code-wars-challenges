de1002c195c77a37575b8f7cea5d0364d5ced3e9
Marija H <93047044+mh1251@users.noreply.github.com>
2021-12-07 09:03:44 +0100

    Update nextSmallerNumber.js

--- nextSmallerNumber.js
+++ nextSmallerNumber.js
@@ -5,21 +5,22 @@ function nextSmaller(n) {
     let finalNumber = ''
     let finalNumberLength;
     for (let i = n.length - 1; i > 0; i--) {
-
+//find the first digit that has one smaller digit next to its right
         if (n[i] < n[i - 1]) {
             let currentNumber = n[i - 1];
             console.log('CURRENT', currentNumber)
             let currentIndex = i - 1;
             console.log('CURRENT INDEX', currentIndex)
+//when we find the digit we slice the array in two part the first that has the swappable number and second which needs to be sorted
             let splicedArray = n.splice(0, currentIndex + 1);
             console.log('NEW ARRAY', splicedArray)
             n.slice(currentIndex + 1, n.length)
-
+//go sortirame vtoriot del od arrayot za da najdeme najgolemiot digitnshto e pomal od digitot na currentIndex (toj shto treba da se swapne)
             n.sort(function (a, b) { return a < b ? 1 : -1 })
             console.log('OLD ARR', n)
             n = splicedArray.concat(n)
             console.log('final array', n)
-
+//go barame brojot shto e pomal od currentIndex digit-ot
             for (let j = currentIndex + 1; j < n.length; j++) {
                 console.log('current', currentIndex)
                 if (n[j] < n[currentIndex]) {
@@ -31,6 +32,7 @@ function nextSmaller(n) {
                     break;
                 }
             }
+ // proveruvame dali e pomal vo sluchaj na nuli (primer za 1027 sleden broj mozhe da go dade deka e pomal 0721 shto ne e tochno
             if (parseInt(finalNumber) < parseInt(unchangableArr.join(''))
                 && finalNumberLength === unchangableArr.join('').length) {
                 return parseInt(finalNumber)
